8db580d685f91c741d9123a2030fc2811b97003d
Fahmi <fahmi@appwapp.com>
2022-05-04 22:29:41 -0400

    Mise en place du footer + du css

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <div id="app">
     <main-menu></main-menu>
-    <router-view></router-view>
+      <div id="wrapper">
+        <router-view></router-view>
+      </div>
+    <footer-app></footer-app>
   </div>
 </template>
 
 <script>
 import MainMenu from './components/MainMenu.vue';
+import FooterApp from './components/FooterApp.vue';
 export default {
   name: 'App',
   components: {
-    MainMenu
+    MainMenu,
+    FooterApp
   }
 }
 </script>
 <style>
   @import url('assets/css/app.css');
+  @import url('assets/css/login.css');
   @import'~bootstrap/dist/css/bootstrap.css'
 </style>
