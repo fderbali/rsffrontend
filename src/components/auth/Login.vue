1ab6af87b5a79b0a5295d62e992d75c1c76bcaf1
Fahmi <fahmi@appwapp.com>
2022-05-07 00:51:26 -0400

    Enlever label remember me

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -11,11 +11,6 @@
                     <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
                     <label for="floatingPassword">Password</label>
                 </div>
-                <div class="checkbox mb-3">
-                    <label>
-                    <input type="checkbox" value="remember-me"> Remember me
-                    </label>
-                </div>
                 <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
             </form>
         </main>
