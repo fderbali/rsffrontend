2f2bfddee12967cdf38d851fb5506241c5331446
Fahmi <fahmi@appwapp.com>
2022-05-07 00:53:49 -0400

    Reformat code

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -1,12 +1,10 @@
 <template>
-
   <footer
-          class="text-center text-lg-start text-white"
-          style="background-color: #3e4551"
-          >
+      class="text-center text-lg-start text-white"
+      style="background-color: #3e4551"
+  >
     <!-- Grid container -->
     <div class="container p-4 pb-0">
-
       <!-- Section: CTA -->
       <section class="">
         <p class="d-flex justify-content-center align-items-center">
@@ -18,51 +16,52 @@
       </section>
       <!-- Section: CTA -->
 
-      <hr class="mb-4" />
+      <hr class="mb-4"/>
 
       <!-- Section: Social media -->
       <section class="mb-4 text-center">
         <!-- Facebook -->
         <a
-           class="btn btn-outline-light btn-floating m-1"
-           href="#!"
-           role="button"
-           >
+            class="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+        >
           <font-awesome-icon :icon="['fab','facebook']" class="icon alt"/>
         </a>
 
         <!-- Twitter -->
         <a
-           class="btn btn-outline-light btn-floating m-1"
-           href="#!"
-           role="button"
-           ><font-awesome-icon :icon="['fab','twitter']" class="icon alt"/>
+            class="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+        >
+          <font-awesome-icon :icon="['fab','twitter']" class="icon alt"/>
         </a>
 
         <!-- Instagram -->
         <a
-           class="btn btn-outline-light btn-floating m-1"
-           href="#!"
-           role="button"
-           >
-           <font-awesome-icon :icon="['fab','instagram']" class="icon alt"/>
+            class="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+        >
+          <font-awesome-icon :icon="['fab','instagram']" class="icon alt"/>
         </a>
 
         <!-- Linkedin -->
         <a
-           class="btn btn-outline-light btn-floating m-1"
-           href="#!"
-           role="button"
-           >
+            class="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+        >
           <font-awesome-icon :icon="['fab','linkedin']" class="icon alt"/>
         </a>
 
         <!-- Github -->
         <a
-           class="btn btn-outline-light btn-floating m-1"
-           href="#!"
-           role="button"
-           >
+            class="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+        >
           <font-awesome-icon :icon="['fab','github']" class="icon alt"/>
         </a>
       </section>
@@ -72,9 +71,9 @@
 
     <!-- Copyright -->
     <div
-         class="text-center p-3"
-         style="background-color: rgba(0, 0, 0, 0.2)"
-         >
+        class="text-center p-3"
+        style="background-color: rgba(0, 0, 0, 0.2)"
+    >
       © 2022 Copyright:
       <a class="text-white" href="#">reaseudessavoirfaire.com</a>
     </div>
@@ -84,9 +83,7 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
