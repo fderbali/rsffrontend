144722a06f441e63c4e220525e8d30cb758949f6
Fahmi <fahmi@appwapp.com>
2022-05-12 23:13:11 -0400

    Populer liens home + publier formation

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -7,8 +7,8 @@
                 </a>
 
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="#" class="nav-link px-2 link-secondary">{{ $i18n.t('home') }}</a></li>
-                    <li><a href="#" class="nav-link px-2 link-dark">{{ $i18n.t('publish-training') }}</a></li>
+                    <li><a href="/" class="nav-link px-2 link-secondary">{{ $i18n.t('home') }}</a></li>
+                    <li><a href="form-trainings" class="nav-link px-2 link-dark">{{ $i18n.t('publish-training') }}</a></li>
                     <li><a href="#" class="nav-link px-2 link-dark">{{ $i18n.t('publish-announcement') }}</a></li>
                 </ul>
 
