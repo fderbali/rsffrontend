a21d650612bb27de28bd7a61e40949325bb7460a
Fahmi <fahmi@appwapp.com>
2022-05-16 13:26:19 -0400

    Afficher la pagination uniquement lorsque on a plus qu'une page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -16,22 +16,12 @@
         </div>
         <div class="container d-flex align-items-center justify-content-center p-5">
             <nav aria-label="Page navigation example">
-                <ul class="pagination">
-                    <!--li class="page-item">
-                        <a class="page-link" href="#" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                        </a>
-                    </li-->
+                <ul class="pagination" v-if="trainingsLinks.length > 1">
                     <li class="page-item" v-for="trainingsLink, index in trainingsLinks" :key="index">
                         <a class="page-link" href="#">
                             {{ trainingsLink.label }}
                         </a>
                     </li>
-                    <!--li class="page-item">
-                        <a class="page-link" href="#" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                        </a>
-                    </li-->
                 </ul>
             </nav>
         </div>
