c47a034cd76e569effe1ca8cebe1cc2680a3c6db
Fahmi <fahmi@appwapp.com>
2022-05-28 19:38:45 -0400

    Afficher les orders

--- src/components/Lists/ListOrderPerProf.vue
+++ src/components/Lists/ListOrderPerProf.vue
@@ -1,11 +1,33 @@
 <template>
-    <div>
-        <h1>Order list</h1>
+    <div class="container">
+        <div class="w-50">
+            <table class="table table-success table-striped">
+                <tr>
+                    <th>Formation</th>
+                    <th>Prix TTC</th>
+                    <th>Élève</th>
+                    <th>&nbsp;</th>
+                </tr>
+                <tr v-for="order, index in orders" :key="index">
+                    <td>{{order.title}}</td>
+                    <td>{{order.price}}</td>
+                    <td>{{order.orders[0].estimate.demand.user.first_name}} {{order.orders[0].estimate.demand.user.last_name}} </td>
+                    <td><a :href="`/form-session?order=${order.orders[0].id}`" class="btn btn-success border-danger">Céduler les sessions</a></td>
+                </tr>
+            </table>
+        </div>
     </div>
 </template>
 
 <script>
+import {mapState} from "vuex";
+
 export default {
-    name: "ListOrderPerProf"
+    name: "ListOrderPerProf",
+    computed: {
+        ...mapState('core/order', [
+            'orders'
+        ])
+    }
 }
 </script>
