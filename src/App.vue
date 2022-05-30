49fd0fd4fde14bef4fee15da3eca0a0af313a1d4
Fahmi <fahmi@appwapp.com>
2022-05-29 20:41:17 -0400

    Switch entre le user menu et le menu normal selon si on est connecté ou pas

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
-  <div id="app">
-    <main-menu></main-menu>
-      <div id="wrapper">
-        <router-view></router-view>
-      </div>
-    <footer-app></footer-app>
-  </div>
+    <div id="app">
+        <UserMenu v-if="user"></UserMenu>
+        <main-menu v-else></main-menu>
+        <div id="wrapper">
+            <router-view></router-view>
+        </div>
+        <footer-app></footer-app>
+    </div>
 </template>
 
 <script>
 import MainMenu from './components/MainMenu.vue';
 import FooterApp from './components/FooterApp.vue';
+import UserMenu from "@/components/UserMenu";
+import { mapState } from 'vuex';
 export default {
-  name: 'App',
-  components: {
-    MainMenu,
-    FooterApp
-  }
+    name: 'App',
+    components: {
+        MainMenu,
+        FooterApp,
+        UserMenu
+    },
+    computed: {
+        ...mapState('core/auth', [
+            'user'
+        ])
+    }
 }
 </script>
 <style>
