239b228f47e01bfa1e059db1736b7bedfc002f94
Sylvie Ratelle <ratellesylvie1@gmail.com>
2022-05-31 13:39:45 -0400

    formater page about et contact et détruire css login tout le css est dans app.css

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,7 +1,24 @@
 <template>
-    <h1 class="text-center">
-        Page Contact
-    </h1>
+    <body>
+
+        <h1>Contact</h1>
+        <h3>Pour toute information n'hésitez pas à nous contacter:</h3>
+
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="@/assets/images/logo.png" id="logo" alt="logo" style="width:300px;height:300px;">
+                </div>
+                <div class="flip-card-back">
+                    <h1>Réseau su Savoir Faire - RSF</h1> 
+                    <p>Concepteurs ; Raouf, Sylvie, Fahmi</p>
+                    <p>Cellulaire : 514 555-1144</p>
+                    <p>Couriel : rsfrsf2022@gmail.com</p> 
+                </div>
+            </div>
+        </div>
+
+    </body>
 </template>
 
 <script>
