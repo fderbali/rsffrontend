43b2aef8d9cb1816073e2c7bbed3628f40955652
Sylvie Ratelle <ratellesylvie1@gmail.com>
2022-06-01 01:29:39 -0400

    Formater pages about et contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,23 +1,20 @@
 <template>
     <body>
-
-        <h1>Contact</h1>
-        <h3>{{ $i18n.t('msg-info') }}</h3>
-
-        <div class="flip-card">
-            <div class="flip-card-inner">
-                <div class="flip-card-front">
-                    <img src="@/assets/images/logo.png" id="logo" alt="logo" style="width:300px;height:300px;">
-                </div>
-                <div class="flip-card-back">
-                    <h4>{{ $i18n.t('us') }}</h4> 
-                    <p>{{ $i18n.t('concept') }}</p>
-                    <p>{{ $i18n.t('cell') }}</p>
-                    <p>{{ $i18n.t('email-us') }}</p> 
+        <div class="col d-flex justify-content-center pt-5">
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="@/assets/images/logo.png" id="logo" alt="logo" style="width:400px;height:200px;">
+                    </div>
+                    <div class="flip-card-back">
+                        <h4>{{ $i18n.t('us') }}</h4><br> 
+                        <h6>{{ $i18n.t('concept') }}</h6>
+                        <h6>{{ $i18n.t('cell') }}</h6>
+                        <h6>{{ $i18n.t('email-us') }}</h6> 
+                    </div>
                 </div>
             </div>
         </div>
-
     </body>
 </template>
 
